82e6ffd37644bcc4de028338a2a804980a639557
tony <tony.spicer.covin@gmail.com>
2019-05-09 17:10:25 -0500

    working on db

--- client/src/main.js
+++ client/src/main.js
@@ -3,9 +3,13 @@ import BootstrapVue from 'bootstrap-vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
 import router from './router'
+//import vuerouter library
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+//configuring bootstrap
+import billAPIService from '@/services/billService'
 Vue.use(BootstrapVue)
+Vue.prototype.$billAPIService = billAPIService
 Vue.config.productionTip = false
 Vue.use(VueRouter)
 new Vue({
---
+++ client/src/services/billService.js
@@ -0,0 +1,7 @@
+import axios from 'axios'
+
+export default {
+    getBills(){
+        return axios.get()
+    }
+}
